c4d932990856a3333fc2424f981b17bee8af89aa
lisi <1411452095>
2020-04-03 10:05:10 +0800

    使用vant

---
+++ src/views/home/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>主页<van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button></div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>登录页面</div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
