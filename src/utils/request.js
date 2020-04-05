579eca219beed2b5acbdf44686731f7b04824735
lisi <1411452095>
2020-04-05 10:23:06 +0800

    处理token失效问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import JSONBig from 'json-bigint'
 import store from '@/store'
+import router from '@/router'
 // 创建一个新的axios实例，和原来的axios无关
 const instance = axios.create({
   // 构造函数
@@ -27,11 +28,54 @@ instance.interceptors.request.use(function (config) {
 // 响应拦截
 instance.interceptors.response.use(function (response) {
   try {
+    //   将数据结构
     return response.data.data
   } catch (error) {
     return response.data
   }
-}, function (error) {
+}, async function (error) {
+  const toPath = {
+    path: '/login',
+    query: {
+      redirectUrl: router.currentRoute.path
+    }
+  }
+  // 处理token失效
+  if (error.response.status === 401) {
+    // 表明token已经失效
+    try {
+
+    } catch (error) {
+
+    }
+    if (store.state.user.refresh_token) {
+      // 存在refresh_token
+      try {
+        const result = await axios({
+          url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+          method: 'put',
+          headers: {
+            Authorization: `Bearer ${store.state.user.refresh_token}`
+          }
+
+        })
+        store.commit('updateUser',
+          {
+            user: {
+              token: result.data.data.token,
+              refresh_token: store.state.user.refresh_token
+            }
+          })
+        return instance(error.config)
+      } catch (error) {
+        store.commit('clearUser')
+        router.push(toPath)
+      }
+    } else {
+      store.commit('clearUser')
+      router.push(toPath)
+    }
+  }
   return Promise.reject(error)
 })
 export default instance
