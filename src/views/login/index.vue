5bed0b2d2db080f349b6429b88e3818b0f20f3cd
lisi <1411452095>
2020-04-06 10:34:43 +0800

    登录的基础布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="container">
     <van-nav-bar title="登录" left-arrow @click-left="$router.back()"></van-nav-bar>
+ <van-cell-group>
+  <van-field required placeholder="请输入手机号"  label="手机号"></van-field>
+  <van-field required  placeholder="请输入验证码"  label="验证码">
+    <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+  </van-field>
+</van-cell-group>
+<div class="btn-box">
+  <van-button type="info" round  size="small" block>登录</van-button>
+</div>
+
   </div>
 </template>
 
@@ -11,4 +21,8 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.btn-box{
+  width: 80%;
+  margin: 10px auto;
+}
 </style>
